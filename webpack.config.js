0124549b88cd75931fbd8ea0bd376317c4573570
MopuK <stanislavgusev,sv@gmail.com>
2025-03-17 19:56:34 +0300

    Added

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,26 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-    ],
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        open: true,
-    },
-    mode: 'development',
+    mode: "production"
 };
