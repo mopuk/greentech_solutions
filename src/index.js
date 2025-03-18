89222ec40e9e6dd6de41e5d4a9f0d793668eba5e
MopuK <stanislavgusev,sv@gmail.com>
2025-03-18 09:34:35 +0300

    Added to Git

--- src/index.js
+++ src/index.js
@@ -1,127 +1,127 @@
-import "./index.css";
+document.addEventListener("DOMContentLoaded", () => {
+
+    function checkForm(form) {
+        let errors = {};
+        message.innerHTML = "<h1>Successfully signed up</h1><p>We can't wait to work with you!</p>"
+        message.className = "submit-message"
+
+        const nameInput = form.name
+        const surnameInput = form.surname
+        const emailInput = form.email
 
 
-function checkForm(form) {
-    let errors = {};
-    message.innerHTML = "<h1>Successfully signed up</h1><p>We can't wait to work with you!</p>"
-    message.className = "submit-message"
+        const nameReg = /^[A-Za-zÀ-ÿ]+$/;
+        const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    const nameInput = form.name
-    const surnameInput = form.surname
-    const emailInput = form.email
-
-
-    const nameReg = /^[A-Za-zÀ-ÿ]+$/;
-    const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (!nameReg.test(nameInput.value)) {
-        nameInput.style.border = "1px solid red";
-        let err = document.getElementById("name-err");
-        if (!err) {
-            err = document.createElement("div");
-            err.innerHTML = "<b>Name must consist only of letters</b>";
-            err.className = "error-msg";
-            err.id = "name-err"
-            nameInput.after(err);
-            errors = { ...errors, name: err };
+        if (!nameReg.test(nameInput.value)) {
+            nameInput.style.border = "1px solid red";
+            let err = document.getElementById("name-err");
+            if (!err) {
+                err = document.createElement("div");
+                err.innerHTML = "<b>Name must consist only of letters</b>";
+                err.className = "error-msg";
+                err.id = "name-err"
+                nameInput.after(err);
+                errors = { ...errors, name: err };
+            }
+        } else {
+            nameInput.style.border = "none";
+            const err = document.getElementById("name-err");
+            if (err) {
+                err.remove();
+                delete errors.name;
+            }
         }
-    } else {
-        nameInput.style.border = "none";
-        const err = document.getElementById("name-err");
-        if (err) {
-            err.remove();
-            delete errors.name;
+        if (!nameReg.test(surnameInput.value)) {
+            surnameInput.style.border = "1px solid red";
+            let err = document.getElementById("surname-err");
+            if (!err) {
+                err = document.createElement("div");
+                err.innerHTML = "<b>Surname must consist only of letters</b>";
+                err.className = "error-msg";
+                err.id = "surname-err"
+                surnameInput.after(err);
+                errors = { ...errors, surname: err };
+            }
+        } else {
+            surnameInput.style.border = "none";
+            const err = document.getElementById("surname-err");
+            if (err) {
+                err.remove();
+                delete errors.surname;
+            }
+        }
+        if (!emailReg.test(emailInput.value)) {
+            emailInput.style.border = "1px solid red";
+            let err = document.getElementById("email-err");
+            if (!err) {
+                err = document.createElement("div");
+                err.innerHTML = "<b>Incorrect email</b>";
+                err.className = "error-msg email";
+                err.id = "email-err"
+                emailInput.after(err);
+                errors = { ...errors, email: err };
+            }
+        } else {
+            emailInput.style.border = "none"
+            const err = document.getElementById("email-err");
+            if (err) {
+                err.remove();
+                delete errors.email;
+            }
+        }
+
+        if (Array.from(document.getElementsByClassName("error-msg")).length == 0) {
+            background.appendChild(message);
+            document.body.append(background)
         }
     }
-    if (!nameReg.test(surnameInput.value)) {
-        surnameInput.style.border = "1px solid red";
-        let err = document.getElementById("surname-err");
-        if (!err) {
-            err = document.createElement("div");
-            err.innerHTML = "<b>Surname must consist only of letters</b>";
-            err.className = "error-msg";
-            err.id = "surname-err"
-            surnameInput.after(err);
-            errors = { ...errors, surname: err };
-        }
-    } else {
-        surnameInput.style.border = "none";
-        const err = document.getElementById("surname-err");
-        if (err) {
-            err.remove();
-            delete errors.surname;
+
+    const background = document.createElement("div");
+    background.className = "background";
+
+    const contactButton = document.getElementById("contact-button");
+    contactButton.onclick = (e) => {
+        const form = document.forms[0];
+
+        window.scrollTo(form.offsetLeft, form.offsetTop - (form.offsetTop / 4))
+    }
+
+    const clientsList = document.getElementById("clients-list");
+
+    const arrowLeft = document.getElementById("arrowLeft");
+    const arrowRight = document.getElementById("arrowRight");
+
+    arrowLeft.onclick = (e) => {
+
+        const reviewSize = clientsList.firstElementChild.offsetWidth;
+
+        if (clientsList.scrollLeft > reviewSize) {
+            clientsList.scrollLeft -= reviewSize + 16;
+        } else {
+            clientsList.scrollLeft = 0;
         }
     }
-    if (!emailReg.test(emailInput.value)) {
-        emailInput.style.border = "1px solid red";
-        let err = document.getElementById("email-err");
-        if (!err) {
-            err = document.createElement("div");
-            err.innerHTML = "<b>Incorrect email</b>";
-            err.className = "error-msg email";
-            err.id = "email-err"
-            emailInput.after(err);
-            errors = { ...errors, email: err };
-        }
-    } else {
-        emailInput.style.border = "none"
-        const err = document.getElementById("email-err");
-        if (err) {
-            err.remove();
-            delete errors.email;
+    arrowRight.onclick = (e) => {
+
+        const reviewSize = clientsList.firstElementChild.offsetWidth;
+
+        if (clientsList.scrollLeft < clientsList.scrollWidth - reviewSize) {
+            clientsList.scrollLeft += reviewSize + 16;
+        } else {
+            clientsList.scrollLeft = clientsList.scrollWidth;
         }
     }
 
-    if (Array.from(document.getElementsByClassName("error-msg")).length == 0) {
-        background.appendChild(message);
-        document.body.append(background)
-    }
-}
+    const forms = document.forms;
+    const message = document.createElement("div");
 
-const background = document.createElement("div");
-background.className = "background";
+    Array.from(forms).forEach(form => {
 
-const contactButton = document.getElementById("contact-button");
-contactButton.onclick = (e) => {
-    const form = document.forms[0];
+        form.onsubmit = (e) => {
+            e.preventDefault();
 
-    window.scrollTo(form.offsetLeft, form.offsetTop - (form.offsetTop / 4))
-}
-
-const clientsList = document.getElementById("clients-list");
-
-const arrowLeft = document.getElementById("arrowLeft");
-const arrowRight = document.getElementById("arrowRight");
-
-arrowLeft.onclick = (e) => {
-
-    const reviewSize = clientsList.firstElementChild.offsetWidth;
-
-    if (clientsList.scrollLeft > reviewSize) {
-        clientsList.scrollLeft -= reviewSize + 16;
-    } else {
-        clientsList.scrollLeft = 0;
-    }
-}
-arrowRight.onclick = (e) => {
-
-    const reviewSize = clientsList.firstElementChild.offsetWidth;
-
-    if (clientsList.scrollLeft < clientsList.scrollWidth - reviewSize) {
-        clientsList.scrollLeft += reviewSize + 16;
-    } else {
-        clientsList.scrollLeft = clientsList.scrollWidth;
-    }
-}
-
-const forms = document.forms;
-const message = document.createElement("div");
-
-Array.from(forms).forEach(form => {
-
-    form.onsubmit = (e) => {
-        e.preventDefault();
-
-        checkForm(form);
-    }
-});
+            checkForm(form);
+        }
+    });
+})
