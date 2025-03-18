0cfe731d5b74d0224355389af8ec03ed5af3353f
MopuK <stanislavgusev,sv@gmail.com>
2025-03-18 10:17:51 +0300

    added

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,10 @@ module.exports = {
         }),
     ],
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
         clean: true,
+        assetModuleFilename: '[path][name][ext]',
     },
     module: {
         rules: [
